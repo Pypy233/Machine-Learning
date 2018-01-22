2d657bf2ae6e30bdb075ec35c9ac4f7b420362fd
py-task <2529716798@qq.com>
2018-01-22 10:59:13 +0800

    kNN

--- Base/kNN/kNN.py
+++ Base/kNN/kNN.py
@@ -8,3 +8,20 @@ def createDataSet():
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 0.5
+    sqDistance = sum(sqDiffMat, axis=1)
+    distance = sqDistance ** 0.5
+    sortedDistances = distance.argsort()
+    classCount = {}
+
+    for i in range(k):
+        voteIlabel = labels[sortedDistances[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+    return sortedClassCount[0][0]
+
